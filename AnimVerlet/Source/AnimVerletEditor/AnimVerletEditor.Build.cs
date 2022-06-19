32cb150b09e4dfbcbff3f755dd6a9e5e166b0c14
chonjay <chonjay21@gmail.com>
2022-06-19 23:12:14 +0900

    Fix UE5 link error

--- AnimVerlet/Source/AnimVerletEditor/AnimVerletEditor.Build.cs
+++ AnimVerlet/Source/AnimVerletEditor/AnimVerletEditor.Build.cs
@@ -7,5 +7,13 @@ public class AnimVerletEditor : ModuleRules
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
 		PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "AnimGraphRuntime", "AnimGraph", "BlueprintGraph", "Slate", "SlateCore", "Persona", "UnrealEd", "AnimVerlet" });
+		
+		/// UE5 compatibility
+		BuildVersion Version;
+        if (BuildVersion.TryRead(BuildVersion.GetDefaultFileName(), out Version))
+        {
+            if (Version.MajorVersion == 5)
+				PrivateDependencyModuleNames.AddRange(new string[] { "EditorFramework" });
+        }
 	}
 }
